e04a64b9a61523e05349ba185ede711c57021d5f
Davi Ferreira <davistudyreference@gmail.com>
2025-03-14 20:50:41 -0300

    INICAL: Inseridos arquivos que basicos para utilização durante o projeto

---
+++ Algoritimo - Cap1_FarmTech Solutions.py
@@ -0,0 +1,27 @@
+# MENU
+# Boas Vindas
+# print('Bem vindo ao aplicativo da FarmTech Solution\n\n')
+# Definindo variavel de looping
+# LIMPEZA DO TERMINAL
+    # INSERÇÃO DE DADOS
+        # LIMPEZA DO TERMINAL
+            # ESCOLHA A CULTURA
+
+                # MILHO
+                    # CALCULE A AREA PLANTADA 
+                    # CALCULE O MANEJO DE INSUMOS
+                    # ARMAZENE TUDO EM LISTA
+
+                # SOJA
+                    # CALCULE A AREA PLANTADA
+                    # CALCULE O MANEJO DE INSUMOS
+                    # ARMAZENE TUDO EM LISTAS
+
+    # EXIBIÇÃO DE DADOS
+
+    # ATUALIZAÇÃO DE DADOS
+
+    # EXCLUSÃO DE DADOS
+
+    # EXIT
+
