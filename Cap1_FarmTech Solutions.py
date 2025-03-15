000814c4ecafadc656a205b19e4fd990a4944abb
Davi Ferreira <davistudyreference@gmail.com>
2025-03-14 22:05:30 -0300

    SCRIPT: Melhora de processo com utilização de funções

--- Cap1_FarmTech Solutions.py
+++ Cap1_FarmTech Solutions.py
@@ -10,11 +10,65 @@ dados = {'CULTURA':['MILHO', 'SOJA', 'SOJA'],
 # MENU
 # Boas Vindas
 # print('Bem vindo ao aplicativo da FarmTech Solution\n\n')
+
+#FUNÇÃO DESTINADA A EXIBIÇÃO DE DADOS
 def exibir_dados(dicionario):
-    print('\n|INDEX|CULTURA|--AREA--|--INSUMOS--|')
+
+    # Montando cabeçario
+    print('\n|INDEX|CULTURA|-----AREA-----|-----INSUMOS-----|')
+
+    # Montando extrutura de pseudotabela
     for idx in range(0, len(dicionario['CULTURA'])):
-        print(f"|{idx:<4} | {dicionario['CULTURA'][idx]:<5} | {dicionario['AREA'][idx]:<5} | {dicionario['INSUMOS'][idx]:<9} |")
-# Definindo variavel de looping
+        print(f"|{idx:<4} | {dicionario['CULTURA'][idx]:<5} | {dicionario['AREA'][idx]:<12} | {dicionario['INSUMOS'][idx]:<15} |")
+
+#FUNÇÃO DESTINADA CALCULAR A CULTURA
+def inserir_dados(): 
+    # Declarando variaveis globais
+    global area, insumo, manejo_insumo, resp, cultura
+
+    # Declarando variavel de loop
+    resp = 1
+
+    # Input da cultura a ser utilizada
+    cultura = input('DIGITE A CULTURA QUE DESEJA INCLUIR:\nMILHO\nSOJA\nR:').upper()
+
+    while resp == 1:
+
+        # MILHO
+        if cultura == 'MILHO':
+            
+            # Aviso
+            print('\nA cultura escolhida foir o milho.\nA cultura de milho tem a caracteristica de ser cultivada em uma area quadrada com fertilizante.\nA cada m² serão aplicados 100g de fertilizante.')
+
+            # CALCULE A AREA PLANTADA 
+            m = int(input('DIGITE EM METROS O TAMANHO DE UM DOS LADOS DA AREA PARA CALCULAR A AREA EM M²: '))
+            area = round(m ** 2, 2)
+
+            # CALCULE O MANEJO DE INSUMOS
+            insumo = 100
+            manejo_insumo = round(area * insumo,2)
+
+        # SOJA
+        elif cultura == 'SOJA':
+
+            # Aviso
+            print('\nA cultura escolhida foir o soja.\nA cultura de soja tem a caracteristica de ser cultivada em uma area redonda com fertilizante.\nA cada m² serão pulverizados 500ml de defensivo de soja.')
+
+            # CALCULE A AREA PLANTADA
+            r = int(input('DIGITE EM METROS O RAIO DA AREA: '))
+            area = pi * r ** 2
+
+            # CALCULE O MANEJO DE INSUMOS
+            insumo = 500
+            manejo_insumo = round(area * insumo,2)
+
+                
+        
+        else:
+            print('\nDIGITE UMA CULTURA VALIDA')
+
+        resp = int(input('\nDESEJA INSERIR MAIS DADOS\n1-SIM\n2-NAO\nR: '))
+
 while True:
 # LIMPEZA DO TERMINAL
     clear_output()    
@@ -23,59 +77,33 @@ while True:
 
     # INSERÇÃO DE DADOS
     if menu_select == 1:
-        resp = 1
-        # LIMPEZA DO TERMINAL
-        while resp == 1:
-            # ESCOLHA A CULTURA
-            clear_output()
-            cultura = input('DIGITE A CULTURA QUE DESEJA INCLUIR:\nMILHO\nSOJA\nR:').upper()
+        
+        inserir_dados()
 
-                # MILHO
-            if cultura == 'MILHO':
-                print('\nA cultura escolhida foir o milho.\nA cultura de milho tem a caracteristica de ser cultivada em uma area quadrada com fertilizante.\nA cada m² serão aplicados 100g de fertilizante.')
-                    # CALCULE A AREA PLANTADA 
-                v_dist = int(input('DIGITE EM METROS O TAMANHO DE UM DOS LADOS DA AREA PARA CALCULAR A AREA EM M²: '))
-                area = v_dist ** 2
-                    # CALCULE O MANEJO DE INSUMOS
-                insumo = 100
-                manejo_insumo = round(area * insumo,2)
-                    # ARMAZENE TUDO EM LISTAS
-                dados['CULTURA'].append(cultura)
-                dados['AREA'].append(area)
-                dados['INSUMOS'].append(manejo_insumo)
-                # lista = lista.update({cultura:[metros,area,manejo_insumo]})
-                # print(' CULTURA :   M² | AREA | INSUMO\n',dados)
+        # ARMAZENE TUDO EM LISTAS
+        dados['CULTURA'].append(cultura)
+        dados['AREA'].append(area)
+        dados['INSUMOS'].append(manejo_insumo)
 
-                # SOJA
-            elif cultura == 'SOJA':
-                print('\nA cultura escolhida foir o soja.\nA cultura de soja tem a caracteristica de ser cultivada em uma area redonda com fertilizante.\nA cada m² serão pulverizados 500ml de defensivo de soja.')
-                    # CALCULE A AREA PLANTADA
-                v_dist = int(input('DIGITE EM METROS O RAIO DA AREA: '))
-                area = pi * v_dist ** 2
-
-                    # CALCULE O MANEJO DE INSUMOS
-                insumo = 500
-                manejo_insumo = round(area * insumo,2)
-
-                    # ARMAZENE TUDO EM LISTAS
-                dados['CULTURA'].append(cultura)
-                dados['AREA'].append(area)
-                dados['INSUMOS'].append(manejo_insumo)
-                # lista = {cultura:[raio,area,manejo_insumo]}
-                # print(' CULTURA : RAIO | AREA | INSUMO\n',lista)
-
-            else:
-                print('\nDIGITE UMA CULTURA VALIDA')
-            resp = int(input('\nDESEJA INSERIR MAIS DADOS\n1-SIM\n2-NAO\nR: '))
+    
     # EXIBIÇÃO DE DADOS
-    elif menu_select == 2:
+    elif menu_select == 2:        
         exibir_dados(dados)
 
     # ATUALIZAÇÃO DE DADOS
-    # elif menu_select == 3:
+    elif menu_select == 3:
+        idx = int(input('Escolha os dados que deseja alterar: '))
+        inserir_dados()
+
+        dados['CULTURA'][idx].append(cultura)
+        dados['AREA'][idx].append(area)
+        dados['INSUMOS'][idx].append(manejo_insumo)
 
     # # EXCLUSÃO DE DADOS
     # elif menu_select == 4:
     # # EXIT
     elif menu_select == 5:
         exit()
+
+
+
